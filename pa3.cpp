df542178b5134a37c4bfb8f144bc902205aeeb04
Jacob <jacobgonzalez731@gmail.com>
2015-04-06 04:46:17 -0400

    main header file created

--- pa3.cpp
+++ pa3.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-#include <string>
-#include "Tokenizer.hpp"
+#include "pa3.hpp"
 
 using namespace std;
 
---
+++ pa3.hpp
@@ -0,0 +1,8 @@
+#ifndef PA3_H
+#define PA3_H
+
+#include <iostream>
+#include <string>
+#include "Tokenizer.hpp"
+
+#endif
